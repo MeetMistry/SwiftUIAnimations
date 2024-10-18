4be12b8dfdf2cfe146a6b2c46e4ba7a4829f66ca
Meet Mistry <meet.m@simformsolutions.com>
2024-10-18 08:22:45 +0530

    Initial setup

---
+++ SwiftUIAnimation/SwiftUIAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationApp.swift
+//  SwiftUIAnimation
+//
+//  Created by Meet Mistry on 18/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
